c54136a7d2c59dee2c8ecb0434763012e37a145d
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-10-08 00:50:55 +0200

    Template
    template list.html

--- todopython/tasks/views.py
+++ todopython/tasks/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hola')
+    # return HttpResponse('Hola')
+
+    # Vamos a devolver el list.html
+    return render(request, 'tasks/list.html')
