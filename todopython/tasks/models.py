9909fbf91e77c277837f58137755b47691728021
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-10-08 20:02:13 +0200

    Update models.py
    docs

--- todopython/tasks/models.py
+++ todopython/tasks/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 #Campos de la BBDD
 
+#Si accedemos a localhost/admin veremos la nueva tabla creada.
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
