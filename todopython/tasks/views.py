2968397ff2baf52677a35b3330b5bac34a595e01
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-10-08 23:50:10 +0200

    render data in template

--- todopython/tasks/views.py
+++ todopython/tasks/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hola')
 
+    # renderizamos las tasks con sus atributos
+    tasks = Task.objects.all()
+    
+    #iteramos
+    context = {'tasks':tasks}
+
     # Vamos a devolver el list.html
-    return render(request, 'tasks/list.html')
+    return render(request, 'tasks/list.html', context)
