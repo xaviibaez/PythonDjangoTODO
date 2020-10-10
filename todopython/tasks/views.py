1523ec51192cf46281c0920ddc34088381d2186d
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-10-10 21:46:25 +0200

    create item
    create item

--- todopython/tasks/views.py
+++ todopython/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
@@ -13,7 +13,18 @@ def index(request):
     
     form = TaskForm()
 
-    #iteramos
+
+    # Despues de hacer el POST volvera a esta view
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        # Si es valido, guardamos el form en la BBDD
+        if form.is_valid():
+            form.save()
+
+        # hacemos return de la misma template -> redirect
+        return redirect('/')
+
+    # iteramos
     context = {'tasks':tasks, 'form':form}
 
     # Vamos a devolver el list.html
